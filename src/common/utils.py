f20cba0d691a716fab6e9d23dd220ccff0705eab
Nicholas Jimmy Alden <89759682+Nixx162@users.noreply.github.com>
2025-01-14 00:42:18 +0700

    fix: modify generate_update_diff to pick up None values

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,7 +1,7 @@
 def generate_update_diff(model, update_data: dict):
     diff = {}
     for key, after in update_data.items():
-        before = getattr(model, key)
+        before = getattr(model, key, None)
         if before != after:
             diff[key] = {"before": before, "after": after}
     return diff
